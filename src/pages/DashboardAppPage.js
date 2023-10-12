de8b7074df47cfbd812ad573dcd02176098ea94e
jason <hainguyentran14@gmail.com>
2023-10-12 18:02:32 +0700

    change layout

--- src/pages/DashboardAppPage.js
+++ src/pages/DashboardAppPage.js
@@ -520,10 +520,11 @@ export default function DashboardAppPage() {
         <Grid container spacing={3}>
           <Grid item xs={12} sm={4} md={4}>
             <AppWidgetSummary sx={{ mb: 2 }} title="Balance" total={balance} icon={'noto:money-with-wings'} />
-            <AppWidgetSummaryUSD sx={{ mb: 2 }} title="Total Commissions" total={commission} color="info" icon={'flat-color-icons:bullish'} />
+            <AppWidgetSummary sx={{ mb: 2 }} title="Total Deposit" total={prevDeposit} icon={'vaadin:money-deposit'} />
+
           </Grid>
           <Grid item xs={12} sm={4} md={4}>
-            <AppWidgetSummary sx={{ mb: 2 }} title="Total Deposit" total={prevDeposit} icon={'vaadin:money-deposit'} />
+            <AppWidgetSummaryUSD sx={{ mb: 2 }} title="Total Commissions" total={commission} color="info" icon={'flat-color-icons:bullish'} />
             <AppWidgetSummary sx={{ mb: 2 }} title="Total Withdraw" total={prevWithdraw} icon={'vaadin:money-withdraw'} />
           </Grid>
 
